e17537c8d0bb1aafb7c3b38b800ba10c728ca3af
Thilo Goetz <twgoetz@apache.org>
2007-09-14 11:48:32 +0000

    Jira UIMA-565: apply UIMA-565.patch.
    https://issues.apache.org/jira/browse/UIMA-565
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/uima/sandbox/trunk@575639 13f79535-47bb-0310-9956-ffa450edef68

--- CasEditor/src/main/java/org/apache/uima/caseditor/Application.java
+++ CasEditor/src/main/java/org/apache/uima/caseditor/Application.java
@@ -19,12 +19,19 @@
 
 package org.apache.uima.caseditor;
 
+import java.io.File;
+import java.net.URL;
+
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
+import org.eclipse.osgi.service.datalocation.Location;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.internal.ide.ChooseWorkspaceData;
+import org.eclipse.ui.internal.ide.ChooseWorkspaceDialog;
 
 /**
  * This class controls all aspects of the application's execution
@@ -38,23 +45,51 @@ public class Application implements IApplication {
 
     Shell shell = new Shell(display, SWT.ON_TOP);
 
-//    try {
-//      if (!WorkspaceUtil.checkInstanceLocation(shell)) {
-//        Platform.endSplash();
-//        return EXIT_OK;
-//      }
-//    } finally {
-//      if (shell != null) {
-//        shell.dispose();
-//      }
-//    }
+    try {
+
+      Location instanceLocaction = Platform.getInstanceLocation();
+
+      if (instanceLocaction == null) {
+
+        // show an error to the user
+
+        Platform.endSplash();
+        return EXIT_OK;
+      }
+
+      if (instanceLocaction.isSet()) {
+        // everything is fine
+      } else {
+        ChooseWorkspaceData launchData = new ChooseWorkspaceData(instanceLocaction.getDefault());
+
+        new ChooseWorkspaceDialog(null, launchData, false, true).prompt(true);
+
+        String instancePath = launchData.getSelection();
+
+        File workspace = new File(instancePath);
+        if (!workspace.exists()) {
+          workspace.mkdir();
+        }
+
+        String path = workspace.getAbsolutePath().replace(File.separatorChar, '/');
+
+        URL candidateWorkspaceURL = new URL("file", null, path);
+
+        instanceLocaction.setURL(candidateWorkspaceURL, true);
+        launchData.writePersistedData();
+      }
+    } finally {
+      if (shell != null) {
+        shell.dispose();
+      }
+    }
 
     try {
       int returnCode = PlatformUI.createAndRunWorkbench(display, new ApplicationWorkbenchAdvisor());
       if (returnCode != PlatformUI.RETURN_RESTART) {
         return EXIT_OK;
       }
-      
+
       // the OpenWorkspaceAction sets the PROP_EXIT_CODE before the restart
       return EXIT_RELAUNCH.equals(Integer.getInteger(PROP_EXIT_CODE)) ? EXIT_RELAUNCH
               : EXIT_RESTART;
@@ -65,5 +100,4 @@ public class Application implements IApplication {
 
   public void stop() {
   }
-
 }
