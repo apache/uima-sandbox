7bd7aad72e6f962b0cb4a5978aabdac3624b30e8
Michael Baessler <mbaessler@apache.org>
2007-11-26 14:05:59 +0000

    No Jira - update spelling
    git-svn-id: https://svn.apache.org/repos/asf/incubator/uima/sandbox/trunk@598284 13f79535-47bb-0310-9956-ffa450edef68

--- RegularExpressionAnnotator/src/main/java/org/apache/uima/annotator/regex/extension/Normalization.java
+++ RegularExpressionAnnotator/src/main/java/org/apache/uima/annotator/regex/extension/Normalization.java
@@ -21,7 +21,7 @@ package org.apache.uima.annotator.regex.extension;
 
 /**
  * The Normalization interface was add to implement a custom normalization
- * for feature values before they are assigned to an anntoation. 
+ * for feature values before they are assigned to an annotation. 
  */
 public interface Normalization {
 	
