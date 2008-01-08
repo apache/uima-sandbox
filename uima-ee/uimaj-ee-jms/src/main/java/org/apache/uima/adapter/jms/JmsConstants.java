70d7aa6b68a1c809e6cc0c1e4528a58923aea0d9
Marshall Schor <schor@apache.org>
2008-01-08 14:46:28 +0000

    [UIMA-689] initial import under software grant to ASF.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/uima/sandbox/trunk@610008 13f79535-47bb-0310-9956-ffa450edef68

---
+++ uima-ee/uimaj-ee-jms/src/main/java/org/apache/uima/adapter/jms/JmsConstants.java
@@ -0,0 +1,13 @@
+package org.apache.uima.adapter.jms;
+
+public class JmsConstants
+{
+    public static final String JMS_LOG_RESOURCE_BUNDLE = "jms_adapter_messages";
+
+    public static String threadName()
+    {
+    	return Thread.currentThread().getName();
+    }
+
+private static final String copyright = com.ibm.uima.copyright.Copyright.COPYRIGHT;
+}
