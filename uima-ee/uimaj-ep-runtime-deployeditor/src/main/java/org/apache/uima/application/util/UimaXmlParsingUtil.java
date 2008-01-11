015dea5d46449e76b921e30a96c851404d24e322
Marshall Schor <schor@apache.org>
2008-01-11 19:22:48 +0000

    [UIMA-706]
    git-svn-id: https://svn.apache.org/repos/asf/incubator/uima/sandbox/trunk@611271 13f79535-47bb-0310-9956-ffa450edef68

--- uima-ee/uimaj-ep-runtime-deployeditor/src/main/java/org/apache/uima/application/util/UimaXmlParsingUtil.java
+++ uima-ee/uimaj-ep-runtime-deployeditor/src/main/java/org/apache/uima/application/util/UimaXmlParsingUtil.java
@@ -1,11 +1,22 @@
-/**
- * 
- * Project UIMA Tooling
- * 
- * 
- * creation date: Feb 17, 2006, 2:17:48 PM
- * source:  UimaXmlParsingUtil.java
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
+
 package org.apache.uima.application.util;
 
 import java.io.ByteArrayInputStream;
@@ -19,7 +30,6 @@ import org.apache.uima.UIMA_IllegalStateException;
 import org.apache.uima.tools.debug.util.Trace;
 import org.apache.uima.util.InvalidXMLException;
 import org.apache.uima.util.XMLInputSource;
-import org.apache.uima.util.XMLParser;
 import org.apache.uima.util.XMLizable;
 
 
