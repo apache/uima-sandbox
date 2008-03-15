bcb21c998caff414ea823d4017c79059a682b121
Edward Epstein <eae@apache.org>
2008-03-15 18:31:00 +0000

    UIMA-867 commit Burn's patch.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/uima/sandbox/trunk/uima-as@637450 13f79535-47bb-0310-9956-ffa450edef68

--- uima-as-distr/src/main/scripts/runRemoteAsyncAE.cmd
+++ uima-as-distr/src/main/scripts/runRemoteAsyncAE.cmd
@@ -26,5 +26,5 @@
 @setlocal
 @call "%UIMA_HOME%\bin\setUimaClassPath"
 @if "%JAVA_HOME%"=="" (set UIMA_JAVA_CALL=java) else (set UIMA_JAVA_CALL=%JAVA_HOME%\bin\java)
-@"%UIMA_JAVA_CALL%" -cp "%UIMA_CLASSPATH%" "-Djava.util.logging.config.file=%UIMA_LOGGER_CONFIG_FILE%" %UIMA_JVM_OPTS% org.apache.uima.examples.as.RunRemoteAsyncAE %*
+@"%UIMA_JAVA_CALL%" -cp "%UIMA_CLASSPATH%" "-Duima.datapath=%UIMA_DATAPATH%" "-Djava.util.logging.config.file=%UIMA_LOGGER_CONFIG_FILE%" %UIMA_JVM_OPTS% org.apache.uima.examples.as.RunRemoteAsyncAE %*
 :EXIT
--- uima-as-distr/src/main/scripts/runRemoteAsyncAE.sh
+++ uima-as-distr/src/main/scripts/runRemoteAsyncAE.sh
@@ -30,4 +30,4 @@ then
 else
   UIMA_JAVA_CALL="$JAVA_HOME/bin/java"
 fi
-"$UIMA_JAVA_CALL" -cp "$UIMA_CLASSPATH" -Djava.util.logging.config.file=$UIMA_LOGGER_CONFIG_FILE $UIMA_JVM_OPTS org.apache.uima.examples.as.RunRemoteAsyncAE $*
+"$UIMA_JAVA_CALL" -cp "$UIMA_CLASSPATH" "-Duima.datapath=$UIMA_DATAPATH" -Djava.util.logging.config.file=$UIMA_LOGGER_CONFIG_FILE $UIMA_JVM_OPTS org.apache.uima.examples.as.RunRemoteAsyncAE $*
