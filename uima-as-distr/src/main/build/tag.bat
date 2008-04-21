868de55fcebcd13caaa23bddc8e9fbf57e3623e1
Marshall Schor <schor@apache.org>
2008-04-21 20:50:28 +0000

    [UIMA-1009] add script to tag a uima-as release
    git-svn-id: https://svn.apache.org/repos/asf/incubator/uima/sandbox/trunk/uima-as@650253 13f79535-47bb-0310-9956-ffa450edef68

---
+++ uima-as-distr/src/main/build/tag.bat
@@ -0,0 +1,51 @@
+@echo off
+
+REM   Licensed to the Apache Software Foundation (ASF) under one
+REM   or more contributor license agreements.  See the NOTICE file
+REM   distributed with this work for additional information
+REM   regarding copyright ownership.  The ASF licenses this file
+REM   to you under the Apache License, Version 2.0 (the
+REM   "License"); you may not use this file except in compliance
+REM   with the License.  You may obtain a copy of the License at
+REM
+REM    http://www.apache.org/licenses/LICENSE-2.0
+REM
+REM   Unless required by applicable law or agreed to in writing,
+REM   software distributed under the License is distributed on an
+REM   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+REM   KIND, either express or implied.  See the License for the
+REM   specific language governing permissions and limitations
+REM   under the License.
+
+@echo on
+
+@if "%~1"=="" goto usage
+@if not "%3"=="" goto usage
+@set leveldir=%~1-%~2
+@set svnloc=tags/%~1/%leveldir%
+@goto execute
+
+@:usage
+@echo off
+echo Run this command in its directory to tag a uima-as release
+echo This will copy the HEAD in the repository for several projects
+echo Usage: tag level release-candidate
+echo  example tag uima-as-2.2.2 01
+@echo on
+@goto exit
+
+
+@:execute
+@set baseURL=https://svn.apache.org/repos/asf/incubator/uima/uimaj/trunk
+@set asURL=https://svn.apache.org/repos/asf/incubator/uima/sandbox/trunk
+@set tagURL=https://svn.apache.org/repos/asf/incubator/uima/sandbox/%svnloc%
+@set commitMsg=Create Tag for UIMA-AS
+
+svn copy --dry-run %asURL%/uima-as             %tagURL%                    -m "%commitMsg%"
+svn copy --dry-run %baseURL%/uimaj             %tagURL%/uimaj              -m "%commitMsg%"
+svn copy --dry-run %baseURL%/uimaj-distr       %tagURL%/uimaj-distr        -m "%commitMsg%"
+svn copy --dry-run %baseURL%/uimaj-examples    %tagURL%/uimaj-examples     -m "%commitMsg%"
+svn copy --dry-run %baseURL%/uima-docbooks     %tagURL%/uima-docbooks      -m "%commitMsg%"
+svn copy --dry-run %baseURL%/uima-docbook-tool %tagURL%/uima-docbook-tool  -m "%commitMsg%"
+
+@:exit
