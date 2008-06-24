9b7804cc3a4ff71b7e73e739f0446c3494198040
Jörn Kottmann <joern@apache.org>
2008-06-24 22:23:06 +0000

    Create Tag for CasEditor-2.2.2-02 release candidate based on uimaj-2.2.2-incubating
    git-svn-id: https://svn.apache.org/repos/asf/incubator/uima/sandbox/tags/CasEditor-2.2.2@671370 13f79535-47bb-0310-9956-ffa450edef68

---
+++ CasEditor-2.2.2-02/CasEditor/src/main/java/org/apache/uima/caseditor/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.uima.caseditor;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.ui.application.IWorkbenchConfigurer;
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+import org.eclipse.ui.internal.progress.ProgressMonitorJobsDialog;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+  private static final String PERSPECTIVE_ID = "org.apache.uima.caseditor.perspective.NLP";
+
+  @Override
+  public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+    return new ApplicationWorkbenchWindowAdvisor(configurer);
+  }
+
+  @Override
+  public void initialize(IWorkbenchConfigurer configurer) {
+    configurer.setSaveAndRestore(true);
+  }
+
+  @Override
+  public String getInitialWindowPerspectiveId() {
+    return PERSPECTIVE_ID;
+  }
+
+  @Override
+  public void postShutdown() {
+    IRunnableWithProgress runnable = new IRunnableWithProgress() {
+
+      public void run(org.eclipse.core.runtime.IProgressMonitor monitor)
+              throws InvocationTargetException, InterruptedException {
+        try {
+          ResourcesPlugin.getWorkspace().save(true, monitor);
+        } catch (CoreException e) {
+          // fail silently
+        }
+      }
+    };
+
+    try {
+      new ProgressMonitorJobsDialog(null).run(true, false, runnable);
+    } catch (InvocationTargetException e) {
+      // fail silently
+    } catch (InterruptedException e) {
+      // fail silently
+    }
+  }
+}
