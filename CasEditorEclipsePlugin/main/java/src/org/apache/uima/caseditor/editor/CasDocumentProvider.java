1cc63ad0a2c0c20313bab8218e0f5ed504b79c3e
Jörn Kottmann <joern@apache.org>
2008-11-04 20:59:10 +0000

    UIMA-1178: Check in of Cas Editor editor part sources
    git-svn-id: https://svn.apache.org/repos/asf/incubator/uima/sandbox/trunk@711402 13f79535-47bb-0310-9956-ffa450edef68

---
+++ CasEditorEclipsePlugin/main/java/src/org/apache/uima/caseditor/editor/CasDocumentProvider.java
@@ -0,0 +1,163 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.uima.caseditor.editor;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.apache.uima.cas.Type;
+import org.apache.uima.cas.text.AnnotationFS;
+import org.apache.uima.caseditor.editor.annotation.EclipseAnnotationPeer;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.jface.operation.IRunnableContext;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.Position;
+import org.eclipse.jface.text.source.Annotation;
+import org.eclipse.jface.text.source.IAnnotationModel;
+import org.eclipse.jface.text.source.IAnnotationModelListener;
+import org.eclipse.ui.part.FileEditorInput;
+import org.eclipse.ui.texteditor.AbstractDocumentProvider;
+
+/**
+ * Provides the {@link org.apache.uima.caseditor.editor.IDocument} for
+ * the {@link AnnotationEditor}.
+ */
+public abstract class CasDocumentProvider extends AbstractDocumentProvider {
+
+	/**
+	 * The method {@link #createDocument(Object)} put error status
+	 * objects for the given element in this map, if something with document creation
+	 * goes wrong.
+	 *
+	 * The method {@link #getStatus(Object)} can then retrieve and return the status.
+	 */
+	protected Map<Object, IStatus> mElementErrorStatus =
+			new HashMap<Object, IStatus>();
+
+	@Override
+	protected IAnnotationModel createAnnotationModel(Object element) throws CoreException {
+		return new IAnnotationModel() {
+
+			private org.apache.uima.caseditor.editor.IDocument mDocument;
+
+			public void addAnnotation(Annotation annotation, Position position) {
+			}
+
+			public void addAnnotationModelListener(IAnnotationModelListener listener) {
+			}
+
+			public void connect(IDocument document) {
+				mDocument = (org.apache.uima.caseditor.editor.IDocument) document;
+			}
+
+			public void disconnect(IDocument document) {
+				mDocument = null;
+			}
+
+			public Iterator<EclipseAnnotationPeer> getAnnotationIterator() {
+				return new Iterator<EclipseAnnotationPeer>() {
+					private Iterator<AnnotationFS> mAnnotations =
+						mDocument.getCAS().getAnnotationIndex().iterator();
+
+					public boolean hasNext() {
+						return mAnnotations.hasNext();
+					}
+
+					public EclipseAnnotationPeer next() {
+						AnnotationFS annotation = mAnnotations.next();
+
+						EclipseAnnotationPeer peer = new EclipseAnnotationPeer(annotation.getType().getName(), false, "");
+						peer.setAnnotation(annotation);
+						return peer;
+					}
+
+					public void remove() {
+					}};
+			}
+
+			public Position getPosition(Annotation annotation) {
+				EclipseAnnotationPeer peer = (EclipseAnnotationPeer) annotation;
+				AnnotationFS annotationFS = peer.getAnnotationFS();
+				return new Position(annotationFS.getBegin(),
+						annotationFS.getEnd() - annotationFS.getBegin());
+			}
+
+			public void removeAnnotation(Annotation annotation) {
+			}
+
+			public void removeAnnotationModelListener(IAnnotationModelListener listener) {
+			}
+		};
+	}
+
+	/**
+	 * Creates the a new {@link AnnotationDocument} from the given {@link FileEditorInput}
+	 * element. For all other elements null is returned.
+	 */
+	@Override
+	protected abstract IDocument createDocument(Object element) throws CoreException;
+
+	@Override
+	protected abstract void doSaveDocument(IProgressMonitor monitor, 
+			Object element, IDocument document, boolean overwrite) throws CoreException; 
+//  {
+//		fireElementStateChanging(element);
+//
+//		org.apache.uima.caseditor.editor.IDocument casDocument =
+//			(org.apache.uima.caseditor.editor.IDocument) document;
+//
+//		try {
+//			casDocument.save();
+//		}
+//		catch (CoreException e) {
+//			fireElementStateChangeFailed(element);
+//			throw e;
+//		}
+//
+//		fireElementDirtyStateChanged(element, false);
+//	}
+
+	@Override
+	protected IRunnableContext getOperationRunner(IProgressMonitor monitor) {
+		return null;
+	}
+
+	@Override
+	public IStatus getStatus(Object element) {
+	    IStatus status = mElementErrorStatus.get(element);
+
+	    if (status == null) {
+	      status = super.getStatus(element);
+	    }
+
+	    return status;
+	}
+
+	protected abstract AnnotationStyle getAnnotationStyle(Object element, Type type);
+	
+	protected abstract EditorAnnotationStatus getEditorAnnotationStatus(
+			Object element);
+
+	protected abstract void setEditorAnnotationStatus(Object element,
+			EditorAnnotationStatus editorAnnotationStatus);
+}
---
+++ CasEditorEclipsePlugin/main/java/src/org/apache/uima/caseditor/editor/CasEditorPlugin.java
@@ -0,0 +1,144 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.uima.caseditor.editor;
+
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * TODO: add javadoc here
+ */
+public class CasEditorPlugin extends AbstractUIPlugin {
+  public static final String ID = "org.apache.uima.caseditor";
+
+  private static final String ICONS_PATH = "icons/";
+
+  /**
+   * The shared instance.
+   */
+  private static CasEditorPlugin sPlugin;
+
+  /**
+   * Resource bundle.
+   */
+  private ResourceBundle mResourceBundle;
+
+  /**
+   * The constructor.
+   */
+  public CasEditorPlugin() {
+    super();
+
+    sPlugin = this;
+  }
+
+  /**
+   * This method is called upon plug-in activation
+   *
+   * @param context
+   * @throws Exception
+   */
+  @Override
+  public void start(BundleContext context) throws Exception {
+    super.start(context);
+  }
+
+  /**
+   * This method is called when the plug-in is stopped.
+   *
+   * @param context
+   * @throws Exception
+   */
+  @Override
+  public void stop(BundleContext context) throws Exception {
+    super.stop(context);
+
+    sPlugin = null;
+    mResourceBundle = null;
+  }
+
+  /**
+   * Returns the shared instance.
+   *
+   * @return the TaePlugin
+   */
+  public static CasEditorPlugin getDefault() {
+    return sPlugin;
+  }
+
+  /**
+   * Returns the string from the plugin's resource bundle, or 'key' if not found.
+   *
+   * @param key
+   * @return resource string
+   */
+  public static String getResourceString(String key) {
+    ResourceBundle bundle = getDefault().getResourceBundle();
+
+    try {
+      return (bundle != null) ? bundle.getString(key) : key;
+    } catch (MissingResourceException e) {
+      return key;
+    }
+  }
+
+  /**
+   * Returns the plugin's resource bundle.
+   *
+   * @return the ResourceBbundle or null if missing
+   */
+  public ResourceBundle getResourceBundle() {
+    try {
+      if (mResourceBundle == null) {
+        mResourceBundle = ResourceBundle.getBundle("Annotator.AnnotatorPluginResources");
+      }
+    } catch (MissingResourceException x) {
+      mResourceBundle = null;
+    }
+
+    return mResourceBundle;
+  }
+
+  /**
+   * Log the throwable.
+   *
+   * @param t
+   */
+  public static void log(Throwable t) {
+    getDefault().getLog().log(new Status(IStatus.ERROR, ID, IStatus.OK, t.getMessage(), t));
+  }
+
+  /**
+   * Retrieves an image.
+   *
+   * @param image
+   * @return the requested image if not available null
+   */
+  public static ImageDescriptor getTaeImageDescriptor(Images image) {
+    return imageDescriptorFromPlugin(ID, ICONS_PATH + image.getPath());
+  }
+
+}
