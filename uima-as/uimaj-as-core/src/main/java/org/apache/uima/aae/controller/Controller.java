7fa7c28bb040e4cf14d286108d44d630023eb119
Jaroslaw Cwiklik <cwiklik@apache.org>
2009-04-10 14:41:50 +0000

    UIMA-1109 Added call to quiesceAndStop() when completeProceesingAndStop is called
    git-svn-id: https://svn.apache.org/repos/asf/incubator/uima/sandbox/trunk@763952 13f79535-47bb-0310-9956-ffa450edef68

--- uima-as/uimaj-as-core/src/main/java/org/apache/uima/aae/controller/Controller.java
+++ uima-as/uimaj-as-core/src/main/java/org/apache/uima/aae/controller/Controller.java
@@ -19,7 +19,12 @@
 
 package org.apache.uima.aae.controller;
 
+import org.apache.uima.UIMAFramework;
+import org.apache.uima.aae.UIMAEE_Constants;
+import org.apache.uima.util.Level;
+
 public class Controller implements ControllerMBean{
+  private static final Class CLASS_NAME = ControllerMBean.class;
 
   /**
    * 
@@ -32,10 +37,21 @@ public class Controller implements ControllerMBean{
   }
   public void completeProcessingAndStop() {
     System.out.println("************> Controller:"+controller.getComponentName()+" JMX MBean Received Stop Command");
-    //controller.quiesceAndStop();
+    if (UIMAFramework.getLogger(CLASS_NAME).isLoggable(Level.INFO)) {
+      UIMAFramework.getLogger(CLASS_NAME).logrb(Level.INFO, CLASS_NAME.getName(),
+                 "completeProcessingAndStop", UIMAEE_Constants.JMS_LOG_RESOURCE_BUNDLE, "UIMAEE_jmx_stop_called__INFO",
+                 new Object[] { controller.getComponentName() });
+    }
+
+    controller.quiesceAndStop();
   }
 
   public void stopNow() {
+    if (UIMAFramework.getLogger(CLASS_NAME).isLoggable(Level.INFO)) {
+      UIMAFramework.getLogger(CLASS_NAME).logrb(Level.INFO, CLASS_NAME.getName(),
+                 "stopNow", UIMAEE_Constants.JMS_LOG_RESOURCE_BUNDLE, "UIMAEE_jmx_stopNow_called__INFO",
+                 new Object[] { controller.getComponentName()});
+    }
     controller.terminate();
   }
   
