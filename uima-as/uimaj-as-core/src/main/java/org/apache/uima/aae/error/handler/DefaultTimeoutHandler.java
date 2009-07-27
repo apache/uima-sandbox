33231598dd02da69a7cd1d8cf776fe011300c910
Jaroslaw Cwiklik <cwiklik@apache.org>
2009-07-27 14:31:57 +0000

    UIMA-1459 Removed dead store in casReferenceId. This is never used
    git-svn-id: https://svn.apache.org/repos/asf/incubator/uima/sandbox/trunk@798159 13f79535-47bb-0310-9956-ffa450edef68

--- uima-as/uimaj-as-core/src/main/java/org/apache/uima/aae/error/handler/DefaultTimeoutHandler.java
+++ uima-as/uimaj-as-core/src/main/java/org/apache/uima/aae/error/handler/DefaultTimeoutHandler.java
@@ -67,7 +67,6 @@ public class DefaultTimeoutHandler extends ErrorHandlerBase implements ErrorHand
 							}
 							else
 							{
-								String casReferenceId = (String)anErrorContext.get( AsynchAEMessage.CasReference);
 								String action = getAction(Monitor.MetadataRequestTimeoutCount, endpoint.getEndpoint());
 
 								if ( ErrorHandler.DISABLE.equalsIgnoreCase(action))
