60cdcf71c55361ac9208ec3be722f4424924a48e
Jaroslaw Cwiklik <cwiklik@apache.org>
2009-07-27 14:21:04 +0000

    UIMA-1459 Removed dead code
    git-svn-id: https://svn.apache.org/repos/asf/incubator/uima/sandbox/trunk@798150 13f79535-47bb-0310-9956-ffa450edef68

--- uima-as/uimaj-as-core/src/main/java/org/apache/uima/aae/UimaEEShutdownHook.java
+++ uima-as/uimaj-as-core/src/main/java/org/apache/uima/aae/UimaEEShutdownHook.java
@@ -25,28 +25,12 @@ import org.apache.uima.aae.jmx.JmxManager;
 
 public class UimaEEShutdownHook extends Thread
 {
-	private JmxManager[] jmxMgrs;
-	private String serviceName;
 	
 	public UimaEEShutdownHook( ArrayList jmxManagers, String aServiceName )
 	{
-		jmxMgrs = new JmxManager[jmxManagers.size()];
-		jmxManagers.toArray(jmxMgrs);
-		serviceName = aServiceName;
 	}
 	public void run() 
 	{
-//        try
-//        {
-//        	for( int i=0; jmxMgrs!= null && i < jmxMgrs.length; i++ )
-//        	{
-//            	jmxMgrs[i].removeQueue();
-//        	}
-//        }
-//        catch( Exception e)
-//        {
-//        	e.printStackTrace();
-//        }
 	}
 
 
